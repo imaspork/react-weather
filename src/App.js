869fdfa36b8414a73cd96de35c82a54f56be50b5
imaspork <tyler.strong1@gmail.com>
2021-07-19 18:18:45 -0700

    first working iteration
    dynamic background, and image properties
    
    localstorage obj usage for persistence (not implemented)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
-
 import "./App.css";
 import axios from "axios";
+import excellent from "../src/excellent.svg";
+import good from "../src/good.svg";
+import bad from "../src/bad.svg";
+import veryBad from "../src/veryBad.svg";
+import terrible from "../src/terrible.svg";
+import threatening from "../src/threatening.svg";
 require("dotenv").config();
 
 const Weather = () => {
 	const [userCity, setCity] = useState("");
 	const [userState, setState] = useState("");
 	const [userWeather, setUserWeather] = useState([]);
-
 	const onChangeHandlerCity = (event) => {
 		event.preventDefault();
 		setCity(event.target.value);
@@ -26,17 +30,80 @@ const Weather = () => {
 					`http://api.airvisual.com/v2/city?city=${userCity}&state=${userState}&country=USA&key=${process.env.REACT_APP_API_KEY}`
 				)
 				.then((response) => {
-					setUserWeather({
+					let weatherObj = {
+						location: `${userCity}, ${userState}`,
 						aqius: response.data.data.current.pollution.aqius,
 						temperature: response.data.data.current.weather.tp,
 						humidity: response.data.data.current.weather.hu,
+					};
+					localStorage.setItem(
+						"Weather-Obj",
+						JSON.stringify(weatherObj)
+					);
+
+					setUserWeather({
+						aqius: weatherObj.aqius,
+						temperature: weatherObj.temperature,
+						humidity: weatherObj.humidity,
 					});
 				});
 		}
 	};
-	// currently first call does not work because react fetches the info before it's set in state - look into adding .5 to 1 second delay
+
+	function airQualityImage() {
+		if (userWeather.aqius <= 50) {
+			return excellent;
+		} else {
+			if (userWeather.aqius >= 51 && userWeather.aqius <= 100) {
+				return good;
+			} else {
+				if (userWeather.aqius >= 101 && userWeather.aqius <= 150) {
+					return bad;
+				} else {
+					if (userWeather.aqius >= 151 && userWeather.aqius <= 200) {
+						return veryBad;
+					} else {
+						if (
+							userWeather.aqius >= 201 &&
+							userWeather.aqius <= 300
+						) {
+							return terrible;
+						} else {
+							return threatening;
+						}
+					}
+				}
+			}
+		}
+	}
+	function airQualityText() {
+		if (userWeather.aqius <= 50) {
+			return "excellent";
+		} else {
+			if (userWeather.aqius >= 51 && userWeather.aqius <= 100) {
+				return "good";
+			} else {
+				if (userWeather.aqius >= 101 && userWeather.aqius <= 150) {
+					return "bad";
+				} else {
+					if (userWeather.aqius >= 151 && userWeather.aqius <= 200) {
+						return "veryBad";
+					} else {
+						if (
+							userWeather.aqius >= 201 &&
+							userWeather.aqius <= 300
+						) {
+							return "terrible";
+						} else {
+							return "threatening";
+						}
+					}
+				}
+			}
+		}
+	}
 	return (
-		<div id="contain-all">
+		<div id="contain-all" className={airQualityText()}>
 			<div id="form-container">
 				<form onSubmit>
 					<label id="label">
@@ -62,14 +129,17 @@ const Weather = () => {
 			</div>
 			<div>
 				{!userWeather.aqius ? (
-					<div>loading... </div>
+					<div></div>
 				) : (
 					<div>
-						<div>{userWeather.aqius}</div>
-						<div>{userWeather.temperature}</div>
-						<div>{userWeather.humidity}</div>
+						<div>Air Quality: {userWeather.aqius}</div>
+						<div>Temperature: {userWeather.temperature}c&deg;</div>
+						<div>Humidity: {userWeather.humidity}%</div>
 					</div>
 				)}
+			</div>
+			<div>
+				<img src={!userWeather.aqius ? null : airQualityImage()}></img>
 			</div>
 		</div>
 	);
