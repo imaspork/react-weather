1cb683da67ff632b6f888d1301beaf985a4715e9
imaspork <tyler.strong1@gmail.com>
2021-07-22 18:58:37 -0700

    code cleanup - design changes
    focused on changing the design and implementing an AQI graph with dropdrown

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {
 	Input,
 	Stack,
-	Icon,
 	InputGroup,
 	InputLeftElement,
 	Button,
@@ -14,7 +13,16 @@ import {
 	IconButton,
 	Text,
 	Flex,
+	Box,
+	Grid,
+	GridItem,
+	Accordion,
+	AccordionItem,
+	AccordionButton,
+	AccordionPanel,
+	AccordionIcon,
 } from "@chakra-ui/react";
+
 import { EditIcon } from "@chakra-ui/icons";
 import { FaSun, FaMoon } from "react-icons/fa";
 import "./App.css";
@@ -63,6 +71,7 @@ const Weather = () => {
 						temperature: response.data.data.current.weather.tp,
 						humidity: response.data.data.current.weather.hu,
 					};
+					console.log(response);
 					setFetchStatus("success");
 					localStorage.setItem(
 						"Weather-Obj",
@@ -82,59 +91,24 @@ const Weather = () => {
 				});
 		}
 	};
-
 	function airQualityImage() {
-		if (userWeather.aqius <= 50) {
+		if (fetchStatus === "failure") {
+			return;
+		} else if (userWeather.aqius <= 50) {
 			return <SvgExcellent fill="#a2ef61" />;
+		} else if (userWeather.aqius >= 51 && userWeather.aqius <= 100) {
+			return <SvgGood fill="#ffec50" />;
+		} else if (userWeather.aqius >= 101 && userWeather.aqius <= 150) {
+			return <SvgBad fill="#ffb14c" />;
+		} else if (userWeather.aqius >= 151 && userWeather.aqius <= 200) {
+			return <SvgVeryBad fill="#ff6275" />;
+		} else if (userWeather.aqius >= 201 && userWeather.aqius <= 300) {
+			return <SvgTerrible fill="#b36ebe" />;
 		} else {
-			if (userWeather.aqius >= 51 && userWeather.aqius <= 100) {
-				return <SvgGood fill="#ffec50" />;
-			} else {
-				if (userWeather.aqius >= 101 && userWeather.aqius <= 150) {
-					return <SvgBad fill="#ffb14c" />;
-				} else {
-					if (userWeather.aqius >= 151 && userWeather.aqius <= 200) {
-						return <SvgVeryBad fill="#ff6275" />;
-					} else {
-						if (
-							userWeather.aqius >= 201 &&
-							userWeather.aqius <= 300
-						) {
-							return <SvgTerrible fill="#b36ebe" />;
-						} else {
-							return <SvgThreatening fill="#955476" />;
-						}
-					}
-				}
-			}
+			return <SvgThreatening fill="#955476" />;
 		}
 	}
-	function airQualityText() {
-		if (userWeather.aqius <= 50) {
-			return "excellent";
-		} else {
-			if (userWeather.aqius >= 51 && userWeather.aqius <= 100) {
-				return "good";
-			} else {
-				if (userWeather.aqius >= 101 && userWeather.aqius <= 150) {
-					return "bad";
-				} else {
-					if (userWeather.aqius >= 151 && userWeather.aqius <= 200) {
-						return "veryBad";
-					} else {
-						if (
-							userWeather.aqius >= 201 &&
-							userWeather.aqius <= 300
-						) {
-							return "terrible";
-						} else {
-							return "threatening";
-						}
-					}
-				}
-			}
-		}
-	}
+
 	const { colorMode, toggleColorMode } = useColorMode();
 
 	function checkFetchStatus() {
@@ -145,14 +119,28 @@ const Weather = () => {
 		}
 	}
 
+	function apiFetchToggle() {
+		apiFetch();
+	}
+
+	function checkStatusChangeRender() {
+		if (!userWeather.aqius) {
+			return null;
+		} else if (fetchStatus === "false") {
+			return null;
+		} else {
+			return airQualityImage();
+		}
+	}
+
 	return (
 		<VStack>
 			<Flex alignSelf="flex-end" p="3">
 				<IconButton
 					isRound={true}
 					size="lg"
-					alignSelf="flex-end"
-					icon={<FaSun />}
+					icon={colorMode === "light" ? <FaSun /> : <FaMoon />}
+					boxShadow="inner"
 					onClick={toggleColorMode}
 				></IconButton>
 			</Flex>
@@ -162,62 +150,250 @@ const Weather = () => {
 					Check Local Air Quality
 				</Text>
 			</Container>
-			<Container>
-				<form action="submit">
-					<Stack spacing={3}>
-						<FormControl isRequired>
-							<InputGroup>
-								<InputLeftElement children={<EditIcon />} />
-								<Input
-									type="city"
-									placeholder="City"
-									aria-label="City"
-									onChange={onChangeHandlerCity}
-									value={userCity}
-								/>
-							</InputGroup>
-						</FormControl>
-						<FormControl isRequired>
-							<InputGroup>
-								<InputLeftElement children={<EditIcon />} />
-								<Input
-									type="state"
-									placeholder="State"
-									aria-label="State"
-									onChange={onChangeHandlerState}
-									value={userState}
-								/>
-							</InputGroup>
-						</FormControl>
-						<Divider />
-						<Button variant="solid" onClick={apiFetch}>
-							Check Quality
-						</Button>
-						<Text as="i" textAlign={"center"} color="red.500">
-							{checkFetchStatus()}
-						</Text>
-					</Stack>
-				</form>
+			<Container maxW="container.md">
+				<Container maxW="container.sm">
+					<form action="submit">
+						<Stack spacing={3}>
+							<FormControl isRequired>
+								<InputGroup>
+									<InputLeftElement children={<EditIcon />} />
+									<Input
+										type="city"
+										placeholder="City"
+										aria-label="City"
+										onChange={onChangeHandlerCity}
+										value={userCity}
+									/>
+								</InputGroup>
+							</FormControl>
+							<FormControl isRequired>
+								<InputGroup>
+									<InputLeftElement children={<EditIcon />} />
+									<Input
+										type="state"
+										placeholder="State"
+										aria-label="State"
+										onChange={onChangeHandlerState}
+										value={userState}
+									/>
+								</InputGroup>
+							</FormControl>
+							<Divider />
+							<Button
+								variant="solid"
+								boxShadow="md"
+								onClick={apiFetchToggle}
+							>
+								Check Quality
+							</Button>
+						</Stack>
+					</form>
+				</Container>
 
-				<VStack spacing="4">
-					<Stack p="4">
-						{!userWeather.aqius ? null : airQualityImage()}
-					</Stack>
-					<Text fontSize="4xl">{userWeather.locationCity}</Text>
-					{!userWeather.aqius ? (
-						<div></div>
-					) : (
-						<VStack spacing="4">
-							<div>Air Quality: {userWeather.aqius}</div>
-							<div>
-								Temperature: {userWeather.temperature}c&deg;
-							</div>
-							<div>Humidity: {userWeather.humidity}%</div>
-						</VStack>
-					)}
+				<Box
+					textAlign="center"
+					maxW="container.md"
+					boxShadow="2xl"
+					id="weather-box"
+					p="15px"
+					color="white"
+					mt="4"
+					rounded="md"
+				>
+					<Text as="i" textAlign={"center"} color="red.500">
+						{checkFetchStatus()}
+					</Text>
+					<Accordion color={"gray.500"} allowToggle>
+						<AccordionItem>
+							<h2>
+								<AccordionButton>
+									<Box flex="1" textAlign="center">
+										Air Quality Chart
+									</Box>
+									<AccordionIcon />
+								</AccordionButton>
+							</h2>
+							<AccordionPanel>
+								<Grid
+									textAlign={"center"}
+									h="200px"
+									templateRows="repeat(6, 1fr)"
+									templateColumns="repeat(7, 1fr)"
+									gap={1}
+									rounded={"md"}
+								>
+									<GridItem
+										rounded={"md"}
+										rowStart={1}
+										colSpan={8}
+										colStart={1}
+										bg="blue.500"
+									>
+										<Text fontSize={"x-large"}>
+											Air Quality Index Chart
+										</Text>
+									</GridItem>
 
-					<div></div>
-				</VStack>
+									<GridItem
+										rounded={"md"}
+										colSpan={5}
+										colStart={3}
+										rowStart={2}
+										bg="#a2ef61"
+									>
+										<Text>Good</Text>
+									</GridItem>
+
+									<GridItem
+										rounded={"md"}
+										colSpan={2}
+										colStart={1}
+										rowStart={2}
+										bg="#a2ef61"
+									>
+										<Text>0-51 AQI</Text>
+									</GridItem>
+									<GridItem
+										rounded={"md"}
+										colSpan={5}
+										colStart={3}
+										rowStart={3}
+										bg="#ffec50"
+									>
+										<Text>Moderate</Text>
+									</GridItem>
+									<GridItem
+										rounded={"md"}
+										colSpan={2}
+										colStart={1}
+										rowStart={3}
+										bg="#ffec50"
+									>
+										<Text>51-100 AQI</Text>
+									</GridItem>
+									<GridItem
+										rounded={"md"}
+										colSpan={5}
+										colStart={3}
+										rowStart={4}
+										bg="#ffb14c"
+									>
+										<Text>
+											Unhealthy for Sensitive Groups
+										</Text>
+									</GridItem>
+									<GridItem
+										rounded={"md"}
+										colSpan={2}
+										colStart={1}
+										rowStart={4}
+										bg="#ffb14c"
+									>
+										<Text>101-150 AQI</Text>
+									</GridItem>
+									<GridItem
+										rounded={"md"}
+										colSpan={5}
+										colStart={3}
+										rowStart={5}
+										bg="#ff6275"
+									>
+										<Text>Unhealthy</Text>
+									</GridItem>
+									<GridItem
+										rounded={"md"}
+										colSpan={2}
+										colStart={1}
+										rowStart={5}
+										bg="#ff6275"
+									>
+										<Text>151-200 AQI</Text>
+									</GridItem>
+									<GridItem
+										rounded={"md"}
+										colSpan={5}
+										colStart={3}
+										rowStart={6}
+										bg="#b36ebe"
+									>
+										<Text>Very Unhealthy</Text>
+									</GridItem>
+									<GridItem
+										rounded={"md"}
+										colSpan={2}
+										colStart={1}
+										rowStart={6}
+										bg="#b36ebe"
+									>
+										<Text>201-300 AQI</Text>
+									</GridItem>
+									<GridItem
+										rounded={"md"}
+										colSpan={5}
+										colStart={3}
+										rowStart={7}
+										bg="#955476"
+									>
+										<Text>Hazardous</Text>
+									</GridItem>
+									<GridItem
+										rounded={"md"}
+										colSpan={2}
+										colStart={1}
+										rowStart={7}
+										bg="#955476"
+									>
+										<Text>301-500 AQI</Text>
+									</GridItem>
+								</Grid>
+							</AccordionPanel>
+						</AccordionItem>
+					</Accordion>
+					<Flex spacing="4" p="4" alignItems justifyContent>
+						<Container maxWidth="25%" p="4" centerContent>
+							{checkStatusChangeRender()}
+						</Container>
+						<Container maxWidth="75%">
+							<Grid
+								textAlign="center"
+								color="black"
+								h="75px"
+								templateRows="repeat(4, 1fr)"
+								templateColumns="repeat(2, 1fr)"
+								gap={7}
+							>
+								<GridItem colSpan={1} colStart={1} rowStart={4}>
+									<Text fontSize="x-large">
+										{!userWeather.temperature
+											? ""
+											: `Temperature: ${userWeather.temperature}Cº`}
+									</Text>
+								</GridItem>
+								<GridItem colSpan={1} colStart={1}>
+									<Text fontSize="x-large">
+										{!userWeather.aqius
+											? ""
+											: `AQI: ${userWeather.aqius}`}
+									</Text>
+								</GridItem>
+								<GridItem colSpan={1} colStart={2}>
+									<Text fontSize="x-large">
+										{!userWeather.locationCity
+											? ""
+											: `${userWeather.locationCity}`}
+									</Text>
+								</GridItem>
+								<GridItem rowStart={4} colSpan={1} colStart={2}>
+									<Text fontSize="x-large">
+										{!userWeather.humidity
+											? ""
+											: `Humidity: ${userWeather.humidity}`}
+									</Text>
+								</GridItem>
+							</Grid>
+						</Container>
+					</Flex>
+				</Box>
 			</Container>
 		</VStack>
 	);
--- src/index.js
+++ src/index.js
@@ -16,3 +16,13 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+// } else if (userWeather.aqius >= 101 && userWeather.aqius <= 150) {
+//	setImageState(<SvgBad fill="#ffb14c" />);
+//} else if (userWeather.aqius >= 151 && userWeather.aqius <= 200) {
+//	setImageState(<SvgVeryBad fill="#ff6275" />);
+//} else if (userWeather.aqius >= 201 && userWeather.aqius <= 300) {
+//	setImageState(<SvgTerrible fill="#b36ebe" />);
+//} else {
+//	setImageState(<SvgThreatening fill="#955476" />);
+//}
